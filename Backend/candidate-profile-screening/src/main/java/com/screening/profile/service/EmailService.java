6868125d227fd925373a4242c12e3100fcf6d6b7
VAIBHAV KUMAR <67293081+vaibhavkumar187@users.noreply.github.com>
2025-08-28 01:40:49 +0530

    Update EmailService.java

--- Backend/candidate-profile-screening/src/main/java/com/screening/profile/service/EmailService.java
+++ Backend/candidate-profile-screening/src/main/java/com/screening/profile/service/EmailService.java
@@ -15,7 +15,7 @@ public class EmailService {
 
     public void sendInterviewMail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("vaibhavvirat18@gmail.com");
+        message.setFrom("vaibhavkumar112623@gmail.com");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
