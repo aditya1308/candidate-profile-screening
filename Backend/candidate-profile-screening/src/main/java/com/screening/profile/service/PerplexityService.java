1cf86eb17cefe9f4ed14224cc5f9dff885405fc0
KqOuKiC <sanskqoukai@gmail.com>
2025-08-29 23:24:19 +0530

    Backend Fixes

--- Backend/candidate-profile-screening/src/main/java/com/screening/profile/service/PerplexityService.java
+++ Backend/candidate-profile-screening/src/main/java/com/screening/profile/service/PerplexityService.java
@@ -1,6 +1,5 @@
 package com.screening.profile.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.screening.profile.dto.CandidateReqDTO;
