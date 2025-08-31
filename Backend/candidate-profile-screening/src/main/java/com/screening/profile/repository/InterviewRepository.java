224e548f6ee9b1365a2b6432b6664ab5f864bb2d
Aditya Prasad <asmarty2016@gmail.com>
2025-08-31 11:57:56 +0530

    fix for pending interview

--- Backend/candidate-profile-screening/src/main/java/com/screening/profile/repository/InterviewRepository.java
+++ Backend/candidate-profile-screening/src/main/java/com/screening/profile/repository/InterviewRepository.java
@@ -13,10 +13,21 @@ import java.util.List;
 public interface InterviewRepository extends JpaRepository<Interview, Integer> {
     Optional<Interview> findByJobApplicationId(Long jobAppId);
 
-    @Query("SELECT i FROM Interview i " +
-            "WHERE (i.round1Interviewer.email = :email AND i.round1Details IS NULL) " +
-            "   OR (i.round2Interviewer.email = :email AND i.round2Details IS NULL) " +
-            "   OR (i.round3Interviewer.email = :email AND i.round3Details IS NULL)")
+    @Query(value = """
+        SELECT i.*
+        FROM interview i
+        JOIN admin a ON (
+            i.round1_interviewer_id = a.id
+            OR i.round2_interviewer_id = a.id
+            OR i.round3_interviewer_id = a.id
+        )
+        WHERE a.email = :email
+          AND (
+              i.round1_details IS NULL
+              OR i.round2_details IS NULL
+              OR i.round3_details IS NULL
+          )
+    """, nativeQuery = true)
     List<Interview> findPendingInterviewsForInterviewer(@Param("email") String email);
 
     @Query("SELECT i FROM Interview i " +
